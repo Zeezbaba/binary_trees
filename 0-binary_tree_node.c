b1180c985ec3aa9868e51988bd8951133a2602d5
Zeezbaba <Azeez_okeleji>
2024-01-31 10:54:24 -0800

    New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -20,7 +20,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	}
 
 	newnode->parent = parent;
-	newnode->val = value;
+	newnode->n = value;
 	newnode->left = NULL;
 	newnode->right = NULL;
 
