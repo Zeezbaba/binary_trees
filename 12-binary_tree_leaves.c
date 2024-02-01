65c034da02a75bb1ef821c8eef72cbf0df0f708b
Destiny-Kay <destinykevogo@gmail.com>
2024-02-01 11:41:07 +0300

    Fix: fixed betty errors in the header file

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -17,3 +17,4 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	num_leaves = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
 	return (num_leaves);
 }
+
--- binary_trees.h
+++ binary_trees.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 /**
- * struct binary_trees_s - Binary tree node
+ * struct binary_tree_s - Binary tree node
  *
  * @n: integer stored in the node
  * @parent: Pointer to the parent node
@@ -21,19 +21,19 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
-//binary search tree
+/*binary search tree*/
 typedef struct binary_tree_s bst_t;
 
-//AVL tree
+/*AVL tree*/
 typedef struct binary_tree_s avl_t;
 
-//Max binary Heap
+/*Max binary Heap*/
 typedef struct binary_tree_s heap_t;
 
 /* binary_tree_print.c */
 void binary_tree_print(const binary_tree_t *);
 
-//FUNCS
+/*FUNCS*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -53,5 +53,8 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+/*HELPER FUNCS*/
+int tree_height(const binary_tree_t *tree);
 
 #endif
